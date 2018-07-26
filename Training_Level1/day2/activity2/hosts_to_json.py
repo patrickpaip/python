8da265023b1883448f9d851a09fbf9d97a165df0
Prathik Pai <prathik@logichive.in>
2018-07-26 14:39:14 +0530

    Completed

---
+++ Training_Level1/day2/activity2/hosts_to_json.py
@@ -0,0 +1,26 @@
+import json
+file=open("inputs/hosts")
+temp_list=[]
+for x in file:
+	temp_list.append(x.split())
+file.close()
+# Writing Output1.JSON
+with open("outputs/output1.json","w") as file:
+	list_a=[]
+	for x in temp_list:
+		list_a.append({"url":x[0],"ip":x[1]})
+	file.write(json.dumps(list_a))
+	del list_a
+# Writing Output2.JSON
+with open("outputs/output2.json","w") as file:
+	t_list=list(temp_list)
+	t_list.insert(0,["url","ip"])
+	file.write(json.dumps(t_list))
+	del t_list
+# Writing Output3.JSON
+with open("outputs/output3.json","w") as file:
+	dict_a={}
+	for x in temp_list:
+		dict_a[x[0]]=x[1]
+	file.write(json.dumps(dict_a))
+	del dict_a
