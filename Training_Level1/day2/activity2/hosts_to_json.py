980514f26a1039772371d9d6014a37c8206c085c
Prathik Pai <prathik@logichive.in>
2018-07-26 14:40:32 +0530

    Indent Added

--- Training_Level1/day2/activity2/hosts_to_json.py
+++ Training_Level1/day2/activity2/hosts_to_json.py
@@ -9,18 +9,18 @@ with open("outputs/output1.json","w") as file:
 	list_a=[]
 	for x in temp_list:
 		list_a.append({"url":x[0],"ip":x[1]})
-	file.write(json.dumps(list_a))
+	file.write(json.dumps(list_a,indent=4))
 	del list_a
 # Writing Output2.JSON
 with open("outputs/output2.json","w") as file:
 	t_list=list(temp_list)
 	t_list.insert(0,["url","ip"])
-	file.write(json.dumps(t_list))
+	file.write(json.dumps(t_list,indent=4))
 	del t_list
 # Writing Output3.JSON
 with open("outputs/output3.json","w") as file:
 	dict_a={}
 	for x in temp_list:
 		dict_a[x[0]]=x[1]
-	file.write(json.dumps(dict_a))
+	file.write(json.dumps(dict_a,indent=4))
 	del dict_a
