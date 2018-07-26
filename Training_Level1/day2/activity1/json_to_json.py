4a7c2ba959cd853f9c2f2f675e2b6eba254ecafc
Prathik Pai <prathik@logichive.in>
2018-07-26 14:08:37 +0530

    Comment added

--- Training_Level1/day2/activity1/json_to_json.py
+++ Training_Level1/day2/activity1/json_to_json.py
@@ -1,19 +1,3 @@
-'''
-Create a json file named "settings.json" with the following conditions
-1) "license" An alphanumeric licence key
-2) "username" An user defined username, minimum 6 characters
-3) "time" Time in terms of epoch time
-4) "id" An ID ranging between 0 to 100
-
-Operations:
-1) Check if all the attributes are available in the file
-2) Validate the values
-3) if "id" range is between 0 to 50, append "-add" to the
-   username if not appeneded already
-4) if Operation had any changes to the settings.json file,
-   then write back to the same file, with a operation.log file
-   containing username and time of operation
-'''
 
 import json
 import time
@@ -21,13 +5,19 @@ import time
 file=open("settings.json")
 data=file.read()
 file.close()
+# Convert to Dictionary from received String
 op_content=json.loads(data)
+# Check if attributes available, Note: Here you are checking against the indexes of Dictionary and not value
 if("license" in op_content and "username" in op_content and "time" in op_content and "id" in op_content):
+	# Check the validity of data as per conditions defined, Note: Here we are checking the value against the conditions
 	if(op_content["license"].isalnum() and len(op_content["username"])>=6 and op_content["id"]<100 and op_content["id"]>0):
+		# Check if "-add" contains inside the username
 		if( 0<op_content["id"]<50 and "-add" not in op_content["username"]):
 			op_content["username"]+="-add"
+			# Since the data has been changed, the data in dictionary is converted to string and written to the file 
 			with open("settings.json","w") as file:
 				file.write(json.dumps(op_content))
+			# Logging the data if change occured.
 			with open("operation.log","a") as file:
 				content="change: "+op_content["username"]+" at "+str(time.time())
 				file.write(content+"\n")
